228fddf41d057451db677d202a0a833ea07143a8
Rakeeb Hossain <hossainrakeeb7233@gmail.com>
2023-10-11 12:30:57 -0400

    Initial TG

---
+++ project/project/websetup/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Setup the project application"""
+
+import logging
+
+from project.config.app_cfg import base_config
+
+__all__ = ['setup_app']
+
+log = logging.getLogger(__name__)
+
+from .schema import setup_schema
+from .bootstrap import bootstrap
+
+
+def setup_app(command, conf, vars):
+    """Place any commands to setup project here"""
+    conf = base_config.configure(conf.global_conf, conf.local_conf)
+    base_config.setup(conf)
+
+    setup_schema(command, conf, vars)
+    bootstrap(command, conf, vars)
